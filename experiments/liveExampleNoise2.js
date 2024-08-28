8262972ef20463d49e05428b0ffbc75e1bf238f0
Schmokes <lesa22kz@student.ju.se>
2024-08-28 12:00:05 +0200

    end Live session 1

--- experiments/liveExampleNoise2.js
+++ experiments/liveExampleNoise2.js
@@ -1,11 +1,14 @@
 function setup() {
   createCanvas(600, 600);
+  frameRate(30);
 }
 
-const size = 10;
-const divider = 5;
+const size = 20;
+const divider = 10;
 const numRows = 60;
 const numCols = 60;
+
+let counter = 0;
 
 function draw() {
   background(255);
@@ -14,11 +17,10 @@ function draw() {
   //noiseSeed(8);
   for (let y = 0; y < numRows; y++) {
     for (let x = 0; x < numCols; x++) {
-      const value = noise(x / divider, y / divider) * size;
+      const value = noise(x / divider, y / divider, counter) * size;
       ellipse(size / 2 + x * size, size / 2 + y * size, value);
     }
   }
 
-  //will only draw it once
-  noLoop();
+  counter += 0.3;
 }
