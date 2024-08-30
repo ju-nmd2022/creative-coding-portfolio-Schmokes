cc3b18de2f3d6e3588407a52c5bdd18c6ad73576
Schmokes <lesa22kz@student.ju.se>
2024-08-30 12:50:43 +0200

    fix

--- experiments/growingCircles_02.js
+++ experiments/growingCircles_02.js
@@ -2,21 +2,24 @@ function setup() {
   createCanvas(600, 600);
 }
 
-const size = 10;
-const numRows = 60;
-const numCols = 60;
+const size = 5;
+const numRows = 50;
+const numCols = 50;
 
 function draw() {
-  background(255);
+  background(30);
   noStroke();
-  
+
   fill(169, 69, 213);
   //noiseSeed(8);
   for (let y = 0; y < numRows; y++) {
     for (let x = 0; x < numCols; x++) {
-      const value = map(x, 0, numCols, 2, size * 2);
+      push();
+      translate(170, 170);
+      const value = map(x, 0, numCols, 2, size * 2) / 1.42;
 
       ellipse(size / 2 + x * size, size / 2 + y * size, value);
+      pop();
     }
   }
 
